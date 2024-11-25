dd7b04c744696482538d2a8e60e9391ebe800776
GabTrem <GabrielTremblay09@gmail.com>
2024-11-25 11:18:09 -0500

    json to recipe object

--- app/src/main/java/com/example/ca/csfoy/tp2_sb_gt/model/Recipe.kt
+++ app/src/main/java/com/example/ca/csfoy/tp2_sb_gt/model/Recipe.kt
@@ -10,34 +10,32 @@ data class Recipe (
     val id: Int,
     val title: String,
     val imageUrl: String,
-    val ingredients: List<String>
+    val ingredients: Array<String>
 ) {
     companion object {
         fun recipeFromJson(data: String): Recipe? {
             return null
         }
-        /*
+
        fun recipeListFromJson(data: String): List<Recipe> {
            val recipeListJson = JSONArray(data)
            val recipeList = recipeListJson.let {
                0.until(it.length()).map { index ->
                    val recipe = it.getJSONObject(index)
                    val ingredientsArray = recipe.getJSONArray("extendedIngredients")
-                   var ingredients = mutableListOf<String>()
+                   var ingredients = arrayOf<String>()
 
 
-                   for( in 0 until ingredientsArray.length()){
-                       ingredients.add(ingredientsArray.getString(i))
+                   for(i in 0 until ingredientsArray.length()){
+                       ingredients[i] = ingredientsArray.getJSONObject(i).getString("name");
                    }
 
-                   Recipe(recipe.getInt("id"), recipe.getString("title"), recipe.getString("imageUrl"))
+                   Recipe(recipe.getInt("id"), recipe.getString("title"), recipe.getString("imageUrl"), ingredients)
 
 
                }
            }
            return recipeList
        }
-
-           */
     }
 }
