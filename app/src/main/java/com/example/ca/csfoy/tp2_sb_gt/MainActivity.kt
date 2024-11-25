9d6a0853503b779d46fbbcc725c5123fc2bdaaa3
Samuel B. <2351016@csfoy.ca>
2024-11-25 12:45:42 -0500

    merge commit

--- app/src/main/java/com/example/ca/csfoy/tp2_sb_gt/MainActivity.kt
+++ app/src/main/java/com/example/ca/csfoy/tp2_sb_gt/MainActivity.kt
@@ -16,6 +16,10 @@ import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
 import com.example.ca.csfoy.tp2_sb_gt.database.connectDatabase
 import com.example.ca.csfoy.tp2_sb_gt.screens.Routes
+<<<<<<< Updated upstream
+=======
+import com.example.ca.csfoy.tp2_sb_gt.screens.ShowRecipes
+>>>>>>> Stashed changes
 import com.example.ca.csfoy.tp2_sb_gt.ui.theme.TP2_SamuelBaribault_GabrielTremblayTheme
 
 class MainActivity : ComponentActivity() {
@@ -35,10 +39,18 @@ class MainActivity : ComponentActivity() {
     private fun InitApp(modifier: Modifier) {
         val db = connectDatabase(applicationContext)
 <<<<<<< Updated upstream
+<<<<<<< Updated upstream
+=======
+
+        val viewModel: RecipeViewModel = viewModel(factory = viewModelFactory {
+            initializer { RecipeViewModel(db.favoriteRecipeDao()) }
+        })
+>>>>>>> Stashed changes
 
         val navController = rememberNavController()
         NavHost(navController = navController, startDestination = Routes.Main.title){
             composable(Routes.Main.title){
+<<<<<<< Updated upstream
                 
 =======
         val viewModel: RecipeViewModel = viewModel(factory = viewModelFactory {
@@ -47,6 +59,9 @@ class MainActivity : ComponentActivity() {
         val navController = rememberNavController()
         NavHost(navController = navController, startDestination = Routes.Main.title){
             composable(Routes.Main.title){
+                ShowRecipes(modifier, viewModel)
+>>>>>>> Stashed changes
+=======
                 ShowRecipes(modifier, viewModel)
 >>>>>>> Stashed changes
             }
--- app/src/main/java/com/example/ca/csfoy/tp2_sb_gt/screens/RecipesScreen.kt
+++ app/src/main/java/com/example/ca/csfoy/tp2_sb_gt/screens/RecipesScreen.kt
@@ -8,12 +8,16 @@ import androidx.compose.material3.Card
 import androidx.compose.material3.Surface
 import androidx.compose.runtime.Composable
 <<<<<<< Updated upstream
+<<<<<<< Updated upstream
 import com.example.ca.csfoy.tp2_sb_gt.model.Recipe
 =======
+=======
+>>>>>>> Stashed changes
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
 import com.example.ca.csfoy.tp2_sb_gt.R
 import com.example.ca.csfoy.tp2_sb_gt.service.Recipe
+<<<<<<< Updated upstream
 >>>>>>> Stashed changes
 import com.example.ca.csfoy.tp2_sb_gt.viewModel.RecipeViewModel
 
@@ -29,20 +33,34 @@ fun ShowRecipes(modifier: Modifier, recipeViewModel: RecipeViewModel){
         Column {
             Row{
 
-            }
-        }
-        LazyColumn(modifier = modifier){
-            items(items = recipeViewModel.recipes){ recipe ->
-                RecipeItem(recipe = recipe, onClick = {recipeViewModel.currentRecipe = recipe})
-            }
-        }
+=======
+import com.example.ca.csfoy.tp2_sb_gt.viewModel.RecipeViewModel
 
-    }
-}
 
-@Composable
-fun RecipeItem(recipe: Recipe, onClick: () -> Unit){
-    Card(onClick = onClick) {
-        Text(text = recipe.title)
+
+    @Composable
+    fun ShowRecipes(modifier: Modifier, recipeViewModel: RecipeViewModel) {
+        Surface(modifier = modifier) {
+            Column {
+                Row {
+                    Text(text = stringResource(R.string.recipe_title_main))
+                }
+>>>>>>> Stashed changes
+            }
+            LazyColumn(modifier = modifier) {
+                items(items = recipeViewModel.recipes) { recipe ->
+                    RecipeItem(
+                        recipe = recipe,
+                        onClick = { recipeViewModel.currentRecipe = recipe })
+                }
+            }
+
+        }
     }
-}
+
+    @Composable
+    fun RecipeItem(recipe: Recipe, onClick: () -> Unit) {
+        Card(onClick = onClick) {
+            Text(text = recipe.title)
+        }
+    }
